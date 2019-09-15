46f1ddeafafa557f60fb365cde879d7303bc3952
Andre Alvim <andre@alvim.net>
2019-09-15 17:17:11 -0300

    Middleware de autenticação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,11 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddlewares from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Só vai utilizar o authMiddlewares as rotas abaixo do mesmo
+routes.use(authMiddlewares);
+
+routes.put('/users', UserController.update);
 
 export default routes;
