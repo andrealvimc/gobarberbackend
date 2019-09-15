62f99284e85b70517c2362da5d34d17cb0341d0a
Andre Alvim <andre@alvim.net>
2019-09-15 16:03:39 -0300

    Criando Loaders de models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,7 +2,7 @@ import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
   static init(sequelize) {
-    // super.init é mesma coisa de Model.init sintaxe do es6
+    // super.init é mesma coisa de Model.init sintaxe do es6+
     super.init(
       {
         name: Sequelize.STRING,
