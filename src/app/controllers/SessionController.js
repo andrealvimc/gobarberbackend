c91564cf9d46238754740b67dd34081311287c52
Andre Alvim <andre@alvim.net>
2019-09-16 19:57:03 -0300

    Validando dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,24 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    // Validando dados de entrada.
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    // Validando dados de entrada.
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
