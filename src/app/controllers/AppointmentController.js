2e19a6c8da8c8c32b013a01f8aef200b650c2b66
Andre Alvim <andre@alvim.net>
2019-10-08 22:38:55 -0300

    Configurando templates de e-mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -114,6 +114,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -138,7 +143,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,7 +1,10 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 import mailConfig from '../config/mail';
 
-class mail {
+class Mail {
   constructor() {
     const { host, port, secure, auth } = mailConfig;
 
@@ -11,6 +14,26 @@ class mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {
@@ -21,4 +44,4 @@ class mail {
   }
 }
 
-export default new mail();
+export default new Mail();
